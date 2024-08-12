29926655db6f142440d351484897f9fdfe1ccf7d
Lucia Arias <Lucia Arias@DESKTOP-P5Q5A3L>
2024-08-12 12:01:00 -0300

    Agregar archivos de proyecto.

---
+++ LucyBell_Ventas.BD/Data/Context.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LucyBell_Ventas.BD.Data
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
---
+++ LucyBell_Ventas.BD/Data/Entity/Producto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LucyBell_Ventas.BD.Data.Entity
+{
+    public class Producto : EntityBase
+    {
+        public string Nombre { get; set; }
+
+        public int Stock { get; set; }
+
+        public decimal Precio { get; set; }
+    }
+}
